bc0ea30ec8bdf6ee60537e6f2f0891546191808c
Conny Lin <cornelia.lin@tum.de>
2023-02-05 11:20:36 +0100

    Rename layers + pgup/pgdn on arrow

--- keyboards/keychron/q2/ansi_encoder/keymaps/vodalaptop/keymap.c
+++ keyboards/keychron/q2/ansi_encoder/keymaps/vodalaptop/keymap.c
@@ -19,9 +19,9 @@
 #include "helper_functions.h"
 
 enum layers {
-    WIN_BASE,
-    _FN2,
-    FUN
+    BASE,
+    KB_SYS,
+    FN2
 };
 
 #define KC_TASK LGUI(KC_TAB)
@@ -35,33 +35,33 @@ enum custom_kc {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [WIN_BASE] = LAYOUT_ansi_67(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,    KC_BSPC,          KC_KNOB,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,   KC_BSLS,          KC_DEL,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,             KC_ENT,           KC_POS,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,             KC_RSFT, KC_UP,
-        KC_LCTL, KC_LWIN, KC_LALT,                            KC_SPC,                             KC_RALT, MO(_FN2), MO(FUN),  KC_LEFT, KC_DOWN, KC_RGHT),
+    [BASE] = LAYOUT_ansi_67(
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,    KC_EQL,    KC_BSPC,          KC_KNOB,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,    KC_RBRC,   KC_BSLS,          KC_DEL,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,               KC_ENT,           KC_POS,
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,               KC_RSFT, KC_UP,
+        KC_LCTL, KC_LWIN, KC_LALT,                            KC_SPC,                             KC_RALT, MO(KB_SYS), TT(FN2),   KC_LEFT, KC_DOWN, KC_RGHT),
 
-    [_FN2] = LAYOUT_ansi_67(
+    [KB_SYS] = LAYOUT_ansi_67(
         KC_GRV,  KC_BRID, KC_BRIU, KC_TASK, KC_FLXP, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD,  KC_VOLU,   _______,          RGB_TOG,
         RGB_TOG, RGB_MOD, RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI, _______, _______, _______, _______, _______, _______,  _______,   _______,          _______,
         _______, RGB_RMOD,RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD, _______, _______, _______, _______, _______, _______,             _______,          _______,
         _______,          _______, _______, _______, _______, _______, NK_TOGG, _______, _______, _______, _______,             _______, _______,
         _______, _______, _______,                            _______,                            _______, _______,  _______,   _______, _______, _______),
 
-    [FUN] = LAYOUT_ansi_67(
+    [FN2] = LAYOUT_ansi_67(
         KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,    _______,          QK_BOOT,
         RGB_TOG, RGB_MOD, RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI, _______, _______, _______, _______, _______, _______,  _______,   _______,          _______,
         _______, RGB_RMOD,RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD, _______, _______, _______, _______, _______, _______,             _______,          _______,
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,             _______, _______,
-        _______, _______, _______,                            _______,                            _______, _______,  _______,   KC_HOME, _______, KC_END)
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,             _______, KC_PGUP,
+        _______, _______, _______,                            _______,                            _______, _______,  _______,   KC_HOME, KC_PGDN, KC_END)
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [WIN_BASE] = { ENCODER_CCW_CW(LCTL(LSFT(KC_TAB)), LCTL(KC_TAB)) },
-    [_FN2]     = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
-    [FUN]     = { ENCODER_CCW_CW(_______, _______) },
+    [BASE] = { ENCODER_CCW_CW(LCTL(LSFT(KC_TAB)), LCTL(KC_TAB)) },
+    [KB_SYS]     = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
+    [FN2]     = { ENCODER_CCW_CW(_______, _______) },
 };
 #endif
 
