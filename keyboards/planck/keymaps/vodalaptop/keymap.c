0269f2b12c5d3e36eb7eb183daae69f73f80d325
Conny Lin <cornelia.lin@tum.de>
2023-06-04 17:59:51 +0200

    Yakuake key

--- keyboards/planck/keymaps/vodalaptop/keymap.c
+++ keyboards/planck/keymaps/vodalaptop/keymap.c
@@ -37,14 +37,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |LSftCW|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |RSftCL|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Meta | Alt  | ?    |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * | Ctrl | Meta | Alt  |Yakuak|Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_BASE] = LAYOUT_planck_grid(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     CK_LSCW, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, CK_RSCL,
-    KC_LCTL, KC_LGUI, KC_LALT, KC_NO,   LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LCTL, KC_LGUI, KC_LALT, CK_MYAK, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* INTL Dead Keys (Win Style)
