c359e4a2c94af1f2ac68f5f036c04a80cfee65fe
Conny Lin <cornelia.lin@tum.de>
2023-06-07 22:37:27 +0200

    Enable custom tapping term for LShiftCW

--- keyboards/planck/keymaps/vodalaptop/keymap.c
+++ keyboards/planck/keymaps/vodalaptop/keymap.c
@@ -129,7 +129,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 // Define per-key tapping terms
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        // case CK_LSCW:
+        case CK_LSCW:
         case CK_RSCL:
             return 90;
         default:
